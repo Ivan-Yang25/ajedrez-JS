04d69ee310cbbd7be50f27e2eb548493be42754f
Ivan Yang <yangivan19@gmail.com>
2024-02-09 08:37:57 -0300

    Solucionando lectura de SASS y modificando la logica de la creacion del tablero

--- JS/main.js
+++ JS/main.js
@@ -8,13 +8,74 @@ document.addEventListener('DOMContentLoaded', () => {
         let tablero = document.createElement('DIV');
         cuerpo.appendChild(tablero);
         tablero.classList.add('casilla');
+        
+        switch(true) {
 
-        if(i % 2 === 0) {
+            case i <= 8:
+                
+                negro(tablero, i);
+                break;
 
-            tablero.classList.add('negro');
-        } else {
+            case i > 8 && i <= 16 :
+                
+                blanco(tablero, i);
+                break;
+            
+            case i > 16 && i <= 24 :
+                
+                negro(tablero, i);
+                break;
+            
+            case i > 24 && i <= 32 :
 
-            tablero.classList.add('blanco');
-        };
+                blanco(tablero, i);
+                break;
+
+            case i > 32 && i <= 40 :
+
+                negro(tablero, i);
+                break;
+
+            case i > 40 && i <= 48 :
+
+                blanco(tablero, i);
+                break;
+            
+            case i > 48 && i <= 56 :
+
+                negro(tablero, i);
+                break;
+            
+            case i > 56 && i <= 64 :
+
+                blanco(tablero, i);
+                break;
+
+            default :
+                console.log('Joya');
+                break;
+        }
     };
 });
+
+function negro(tablero, i) {
+
+    if(i % 2 === 0) {
+
+        tablero.classList.add('negro');
+    } else {
+
+        tablero.classList.add('blanco');
+    };
+};
+
+function blanco(tablero, i) {
+
+    if(i % 2 === 0) {
+
+        tablero.classList.add('blanco');
+    } else {
+
+        tablero.classList.add('negro');
+    };
+};
